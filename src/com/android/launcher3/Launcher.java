8cc8a1fbe937e4944f539354495318c14488b412
elpaablo <donopwhile1@gmail.com>
2021-03-17 11:11:29 +0000

    Move restart check to home page
    It's only a flag check. And avoids having to implement an observer to whatch the home key press (and still miss home gesture).

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -1171,6 +1171,8 @@ public class Launcher extends StatefulActivity<LauncherState>
         } else {
             mOverlayManager.onActivityResumed(this);
         }
+                        
+        LauncherAppState.getInstanceNoCreate().checkIfRestartNeeded();
 
         AbstractFloatingView.closeAllOpenViewsExcept(this, false, TYPE_REBIND_SAFE);
         DragView.removeAllViews(this);
