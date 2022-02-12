bf165b8424d09998ee0433b81fd0a6e3d07efcb9
Pranav Vashi <neobuddy89@gmail.com>
2022-02-13 03:38:19 +0530

    Launcher3: Make lens available without lens package
    * Google search package is capable of lens functionality. Who knew? :)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -157,8 +157,9 @@ public final class Utilities {
     public static final String KEY_DOCK_THEME = "pref_dock_theme";
     public static final String GSA_PACKAGE = "com.google.android.googlequicksearchbox";
     public static final String SUGGESTIONS_PACKAGE = "com.google.android.as";
+    public static final String LENS_ACTIVITY = "com.google.android.apps.lens.MainActivity";
     public static final String LENS_SHARE_ACTIVITY = "com.google.android.apps.search.lens.LensShareEntryPointActivity";
-    public static final String LENS_ACTIVITY = "com.google.vr.apps.ornament.app.lens.LensLauncherActivity";
+    public static final String LENS_URI = "google://lens";
 
     /**
      * Returns true if theme is dark.
